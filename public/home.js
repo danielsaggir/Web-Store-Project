9f68d8cb377e52506244d9979efca37191054e1d
maymichaeli <maushe55@gmail.com>
2024-05-15 14:03:20 +0300

    start proj

---
+++ public/home.js
@@ -0,0 +1,13 @@
+// document.getElementById('maniuButton').addEventListener('click', function() {
+//     var maniuNav = document.getElementById('maniu');
+//     if (maniuNav.style.display === 'none' || maniuNav.style.display === '') {
+//         maniuNav.style.display = 'block';
+//     } else {
+//         maniuNav.style.display = 'none';
+//     }
+// });
+
+
+document.getElementById('maniuButton').addEventListener('click', function() {
+    document.getElementById('maniu').style.display = 'block';
+});
