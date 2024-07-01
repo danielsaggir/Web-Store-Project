ffcd0fa4196d88ea5d048ea7ad23c6ea72d94068
maymichaeli <maushe55@gmail.com>
2024-07-01 18:06:56 +0300

    update the home root. when run the server starts with running the home page

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const express= require('express')
+const express = require('express');
+const path = require('path');
 
-const server= express()
+const server = express();
 
-server.use(express.static('public'))
-//server.get('/', index.html)
+// Serve static files from the 'public' directory
+server.use(express.static('public'));
+
+// Set up a route to serve home.html when accessing '/'
+server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+});
 
 server.listen(80, () => {
     console.log('Server is running on http://localhost');
