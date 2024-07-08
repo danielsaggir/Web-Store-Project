81a768e5f3dd6eec943d2ee6e0809cd1b034ef79
maymichaeli <maushe55@gmail.com>
2024-07-08 17:20:33 +0300

    .

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const server = express();
 // Serve static files from the 'public' directory
 server.use(express.static('public'));
 
+// הגדרת חיבור למסד הנתונים MongoDB Atlas
+const mongoURI = 'mongodb+srv://admin:admin@cluster0.hrynjzk.mongodb.net/mydb?retryWrites=true&w=majority';
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB Atlas'))
+    .catch(err => console.error('Could not connect to MongoDB Atlas...', err));
 
 
 // Set up a route to serve home.html when accessing '/'
