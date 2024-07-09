32e6c3008b3d0e2f0a4d91c143b539de952e7576
danielsaggir <danielsaggir@gmail.com>
2024-07-09 16:35:19 +0300

    cvcvc

--- app.js
+++ app.js
@@ -8,3 +8,9 @@ server.use(express.static('public'))
 server.listen(80, () => {
     console.log('Server is running on http://localhost');
 });
+
+
+
+
+mongodb+srv://May:<colman123>@skidiscluster.lfahubt.mongodb.net/
+mongodb+srv://May:<colman123>@skidiscluster.lfahubt.mongodb.net/
