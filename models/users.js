12f0ae6df58b7eab9f867a253ceef68ff42b5bde
tomergol15 <golod505@gmail.com>
2024-07-10 20:25:41 +0300

    create new branch for users, and add schema to atlas

--- models/users.js
+++ models/users.js
@@ -1,15 +1,27 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: {
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    userName: {
         type: String,
         required: true,
         unique: true
     },
     password: {
         type: String,
+        required: true
+    },
+    email: {
+        type: String,
         required: true,
-        unique : true
+        unique: true
     }
 });
 
