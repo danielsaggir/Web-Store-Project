c7a50558637dacafbd8c5295fa5600ba50bc1e89
tomergol15 <golod505@gmail.com>
2024-07-12 11:34:05 +0300

    add a column in the schema accesories

--- models/Accessories.js
+++ models/Accessories.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
+    MyId: {
+        type : Number,
+        required : true
+    },
     name: {
         type: String,
         required: true
