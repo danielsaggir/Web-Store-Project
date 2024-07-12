34e5a58b5f4f0a890a1d76360e23d207e4dc4050
maymichaeli <maushe55@gmail.com>
2024-07-12 10:40:38 +0300

    ranger ejs, install ejs

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+
 const Accessories = require('./models/Accessories');
 const Clothes = require('./models/Clothes');
 const SkiProducts = require('./models/SkiProducts');
 
-const server = express();
-
-// Serve static files from the 'public' directory
-server.use(express.static('public'));
-
-// הגדרת חיבור למסד הנתונים MongoDB Atlas
+// MongoDB Atlas
 const mongoURI = 'mongodb+srv://admin:admin@cluster0.hrynjzk.mongodb.net/';
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -18,20 +14,40 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .catch(err => console.error('Could not connect to MongoDB Atlas...', err));
 
 
+
+const server = express();
+
+// Serve static files from the 'public' directory
+server.use(express.static('public'));
+
+// Set the view engine to EJS
+server.set('view engine', 'ejs');
+server.set('views', path.join(__dirname, 'views'));
+
+
 // Set up a route to serve home.html when accessing '/'
 server.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'home.ejs'));
+    res.render('home'); // home.ejs
 });
-// app.get('/products', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'products.html'));
-// });
 
-// app.get('/product', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'product.html'));
-// });
+// Set up a route to serve products.html when accessing '/products'
+server.get('/products', (req, res) => {
+    res.render('products'); // products.ejs
+});
 
-// const PORT = process.env.PORT || 3000;
-// server.listen(PORT, () => {
-//     console.log('Server is running on ${PORT}');
+// Set up a route to serve product.html when accessing '/product'
+server.get('/product', (req, res) => {
+    res.render('Singelproduct'); // Singelproduct.ejs
+});
+
+
+// Start the server
+const PORT = process.env.PORT || 80;
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// app.listen(80, () => {
+//     console.log('Server is running on http://localhost');
 // });
 
