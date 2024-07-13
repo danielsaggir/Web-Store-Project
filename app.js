0579bed78e938084c083fd2ed85f75c9d83204c4
tomergol15 <golod505@gmail.com>
2024-07-13 21:30:46 +0300

    adding a line that talking about models/users

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const Accessories = require('./models/Accessories');
 const Clothes = require('./models/Clothes');
 const SkiProducts = require('./models/SkiProducts');
-const Users = require('./models/Users');
+const Users = require('./models/users');
 
 // MongoDB Atlas
 const mongoURI = 'mongodb+srv://admin:admin@cluster0.hrynjzk.mongodb.net/';
