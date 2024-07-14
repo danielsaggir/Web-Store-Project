7f42edb0c69188b4922058d67af6097d1a04eb48
tomergol15 <golod505@gmail.com>
2024-07-14 16:36:55 +0300

    ignore this

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,39 +2,44 @@ const Users = require('../models/users');
 // const bcrypt = require('bcrypt');  // need to check if we need this
 
 
-// פונקציה לטיפול בהרשמה
-exports.registerUser = async (req, res) => {
-    try {
-        const { firstName, lastName, username, password } = req.body;
-        const existingUser = await Users.findOne({ username });
-        if (existingUser) {
-            return res.status(400).send('User already exists');
-        }
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = new Users({ firstName, lastName, username, password: hashedPassword });
-        await newUser.save();
-        res.redirect('/'); // הפניה חזרה לעמוד הבית לאחר הרשמה מוצלחת
-    } catch (error) {
-        res.status(500).send('Server error');
-    }
-};
+// // פונקציה לטיפול בהרשמה
+// exports.registerUser = async (req, res) => {
+//     try {
+//         const { firstName, lastName, username, password } = req.body;
+//         const existingUser = await Users.findOne({ username });
+//         if (existingUser) {
+//             return res.status(400).send('User already exists');
+//         }
+//         const hashedPassword = await bcrypt.hash(password, 10);
+//         const newUser = new Users({ firstName, lastName, username, password: hashedPassword });
+//         await newUser.save();
+//         res.redirect('/'); // הפניה חזרה לעמוד הבית לאחר הרשמה מוצלחת
+//     } catch (error) {
+//         res.status(500).send('Server error');
+//     }
+// };
 
 // פונקציה לטיפול בהתחברות
 exports.loginUser = async (req, res) => {
     try {
         const { username, password } = req.body;
+        console.log('Received username:', username);
+        console.log('Received password:', password);
+
         const user = await Users.findOne({ username });
+
         if (!user || user.password !== password) {
+            console.log('Invalid credentials');
             return res.status(400).send('Invalid credentials');
         }
+
+        console.log('Login successful');
         res.redirect('/');
     } catch (error) {
+        console.error('Server error:', error);
         res.status(500).send('Server error');
     }
 };
-
-
-
 
 
 // // פונקציה לטיפול בהרשמה
