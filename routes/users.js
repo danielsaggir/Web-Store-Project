36dc47cd55e4a61dcac1b03bd70ae7a44a426138
tomergol15 <golod505@gmail.com>
2024-07-14 14:04:57 +0300

    funcs for login and register in usercontrollers

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+// routes/users.js
+const express = require('express');
+const router = express.Router();
+const usersController = require('../controllers/usersController'); // נתיב ל-controller
+
+// טיפול בבקשות POST להרשמה
+router.post('/register', usersController.registerUser);
+
+// טיפול בבקשות POST להתחברות
+router.post('/login', usersController.loginUser);
+
+module.exports = router;
