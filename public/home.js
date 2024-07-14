36adbe47b982611127b77ba14b6feb0f51790ea4
tomergol15 <golod505@gmail.com>
2024-07-14 14:04:57 +0300

    funcs for login and register in usercontrollers

--- public/home.js
+++ public/home.js
@@ -104,6 +104,50 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+window.addEventListener('scroll', function() {
+    let topLine = document.getElementById('topLine');
+    let top = document.getElementById('top');
+    let topHeight = top.offsetHeight;
+    if (window.scrollY > topHeight) {
+        topLine.style.backgroundColor = 'rgba(250, 253, 255)';
+    } else {
+        topLine.style.backgroundColor = 'transparent';
+    }
+});
+
+document.getElementById('logInBtn').addEventListener('click', function() {
+    let menu = document.getElementById('logInCard');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+});
+// document.getElementById('logInBtn').addEventListener('click', function() {
+//     let loginCard = document.getElementById('logInCard');
+//     let registerCard = document.getElementById('registerCard');
+//     if (loginCard.style.display === 'none' && registerCard.style.display === 'none') {
+//         loginCard.style.display = 'block';
+//     } else {
+//         loginCard.style.display = 'none';
+//         registerCard.style.display = 'none';
+//     }
+// });
+
+// document.getElementById('showRegisterForm').addEventListener('click', function() {
+//     let loginCard = document.getElementById('logInCard');
+//     let registerCard = document.getElementById('registerCard');
+//     loginCard.style.display = 'none';
+//     registerCard.style.display = 'block';
+// });
+
+// document.getElementById('backToLogin').addEventListener('click', function() {
+//     let loginCard = document.getElementById('logInCard');
+//     let registerCard = document.getElementById('registerCard');
+//     loginCard.style.display = 'block';
+//     registerCard.style.display = 'none';
+// });
+
 
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('ski-products-link').addEventListener('click', () => {
