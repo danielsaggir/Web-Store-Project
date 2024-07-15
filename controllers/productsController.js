aadf4806de534e4029fe455ae67d88eeec5f06f6
danielsaggir <danielsaggir@gmail.com>
2024-07-15 19:36:41 +0300

    Sorting Products by queries

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,8 +2,45 @@ const SkiProducts = require('../models/SkiProducts');
 const Clothes = require('../models/Clothes');
 const Accessories = require('../models/Accessories');
 
+
+
+// exports.getProducts = async (req, res) => {
+//     const selectedCategory = req.query.category; // Renamed to selectedCategory to avoid confusion
+//     console.log('Selected Category:', selectedCategory);
+
+//     let ProductModel;
+
+//     switch (selectedCategory) {
+//         case 'Ski Products':
+//             ProductModel = SkiProducts;
+//             break;
+//         case 'Clothes':
+//             ProductModel = Clothes;
+//             break;
+//         case 'Accessories':
+//             ProductModel = Accessories;
+//             break;
+//         default:
+//             return res.status(400).send('Invalid category');
+//     }
+
+//     try {
+//         const products = await ProductModel.find();
+//         res.render('products', { selectedCategory, products }); // Pass selectedCategory to the view
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send('Internal Server Error');
+//     }
+// };
+
+
 exports.getProducts = async (req, res) => {
-    const selectedCategory = req.query.category; // Renamed to selectedCategory to avoid confusion
+    const selectedCategory = req.query.category;
+    const sortOption = req.query.sort;
+
+    console.log('Selected Category:', selectedCategory);
+    console.log('Sort Option:', sortOption);
+
     let ProductModel;
 
     switch (selectedCategory) {
@@ -20,9 +57,23 @@ exports.getProducts = async (req, res) => {
             return res.status(400).send('Invalid category');
     }
 
+    let sortCriteria = {};
+    switch (sortOption) {
+        case 'price_asc':
+            sortCriteria = { price: 1 };
+            break;
+        case 'price_desc':
+            sortCriteria = { price: -1 };
+            break;
+        //Here you can add another sort option
+        default:
+            sortCriteria = {}; //Default sort is == non sorted
+            break;
+    }
+
     try {
-        const products = await ProductModel.find();
-        res.render('products', { selectedCategory, products }); // Pass selectedCategory to the view
+        const products = await ProductModel.find().sort(sortCriteria).exec();
+        res.render('products', { selectedCategory, products });
     } catch (error) {
         console.error(error);
         res.status(500).send('Internal Server Error');
--- public/home.js
+++ public/home.js
@@ -115,6 +115,7 @@ window.addEventListener('scroll', function() {
     }
 });
 
+
 document.getElementById('logInBtn').addEventListener('click', function() {
     let menu = document.getElementById('logInCard');
     if (menu.style.display === 'block') {
