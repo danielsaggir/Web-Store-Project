c3681f4b04b23c44a7cd8b97a331d565e52c6ded
tomergol15 <golod505@gmail.com>
2024-07-17 21:38:13 +0300

    add route for register

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const usersController = require('../controllers/usersController'); // נתיב ל-controller
 
 // טיפול בבקשות POST להרשמה
-// router.post('/register', usersController.registerUser);
+router.post('/register', usersController.registerUser);
 
 // טיפול בבקשות POST להתחברות
 router.post('/login', usersController.loginUser);
