0e43de9cc8c9c02b53ca0696f6c7b51b24005e7a
tomergol15 <golod505@gmail.com>
2024-07-17 21:37:28 +0300

    ignore changes here

--- models/users.js
+++ models/users.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type : String,
+        type: String,
         required: true
     },
-    lastName : {
-        type : String,
-        required : true
+    lastName: {
+        type: String,
+        required: true
     },
     username: {
         type: String,
@@ -16,8 +16,7 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true,
-        unique : true
+        required: true
     }
 });
 
