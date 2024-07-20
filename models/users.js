3f3118b0499f7f60a29b777d88665d996ad435e5
tomergol15 <golod505@gmail.com>
2024-07-20 21:06:47 +0300

    add column isadmin

--- models/users.js
+++ models/users.js
@@ -17,6 +17,10 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false // ברירת מחדל משתמש רגיל, ניתן לשנות ידנית במידת הצורך
     }
 });
 
