76b07ed9a029615c14685d9950e4b4a347b801e8
tomergol15 <golod505@gmail.com>
2024-07-20 21:07:36 +0300

    change the func here

--- routes/home.js
+++ routes/home.js
@@ -2,12 +2,24 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const Users = require('../models/users'); // Import the Users model
 
 // Route for home
-router.get('/', (req, res) => {
+// router.get('/', (req, res) => {
+//     const { username } = req.query;
+//     res.render('home', { username });
+//     // res.render('home');
+// });
+
+router.get('/', async (req, res) => {
     const { username } = req.query;
-    res.render('home', { username });
-    // res.render('home');
+    if (username) {
+        const user = await Users.findOne({ username });
+        if (user) {
+            return res.render('home', { username: user.username, isAdmin: user.isAdmin });
+        }
+    }
+    res.render('home', { username: 'Guest', isAdmin: false });
 });
 
 
