d6fb2a8f11a669621a29ff2fdd2334c46ff68677
maymichaeli <maushe55@gmail.com>
2024-07-20 18:28:38 +0300

    changes in the top div view

--- public/home.js
+++ public/home.js
@@ -1,24 +1,24 @@
 
-document.getElementById('menuButton').addEventListener('click', function() {
-    let menu = document.getElementById('menu');
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'block';
-    }
-});
+// document.getElementById('menuButton').addEventListener('click', function() {
+//     let menu = document.getElementById('menu');
+//     if (menu.style.display === 'block') {
+//         menu.style.display = 'none';
+//     } else {
+//         menu.style.display = 'block';
+//     }
+// });
 
 
-window.addEventListener('scroll', function() {
-    let topLine = document.getElementById('topLine');
-    let top = document.getElementById('top');
-    let topHeight = top.offsetHeight;
-    if (window.scrollY > topHeight) {
-        topLine.style.backgroundColor = 'rgba(250, 253, 255)';
-    } else {
-        topLine.style.backgroundColor = 'transparent';
-    }
-});
+// window.addEventListener('scroll', function() {
+//     let topLine = document.getElementById('topLine');
+//     let top = document.getElementById('top');
+//     let topHeight = top.offsetHeight;
+//     if (window.scrollY > topHeight) {
+//         topLine.style.backgroundColor = 'rgba(250, 253, 255)';
+//     } else {
+//         topLine.style.backgroundColor = 'transparent';
+//     }
+// });
 
 document.addEventListener('DOMContentLoaded', function() {
     // Function to toggle login card visibility
