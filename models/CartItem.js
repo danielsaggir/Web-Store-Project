4399e242619780ac2c73faed030c31f8fe4e2c0d
tomergol15 <golod505@gmail.com>
2024-07-23 15:23:46 +0300

    me and idan changed the table

--- models/CartItem.js
+++ models/CartItem.js
@@ -1,12 +1,27 @@
 const mongoose = require('mongoose');
 
 const cartItemSchema = new mongoose.Schema({
-    productName: String,
-    productSize: String,
-    productPrice: Number,
-    productImg: String,
-    username: String
-});
+    productName: {
+    type: String,
+    required : true
+    },
+    productSize:{
+        type : String,
+        required: true 
+    },
+    productPrice: {
+        type: Number,
+        required : true
+    },
+    productImg: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required : true
+    }
+    });
 
 const CartItem = mongoose.model('CartItem', cartItemSchema);
 
