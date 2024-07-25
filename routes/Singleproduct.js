f69b8a4bed91d5c3aa0eb586b6b70fa3171cd2f0
maymichaeli <maushe55@gmail.com>
2024-07-25 21:59:20 +0300

    /

--- routes/Singleproduct.js
+++ routes/Singleproduct.js
@@ -5,8 +5,8 @@ const singleProductController = require('../controllers/SingleproductController'
 // Route for single product
 router.get('/SingleProduct', singleProductController.getSingleProduct);
 
-// Cart routes
-router.get('/cart', singleProductController.getCart);
-router.post('/add-to-cart', singleProductController.addToCart);
+// // Cart routes
+// router.get('/cart', singleProductController.getCart);
+// router.post('/add-to-cart', singleProductController.addToCart);
 
 module.exports = router;
