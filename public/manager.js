f3307a1f7feecb2e50312a0a95b764c88c9423c6
danielsaggir <danielsaggir@gmail.com>
2024-07-31 15:34:33 +0300

    סתם משהו

--- public/manager.js
+++ public/manager.js
@@ -458,10 +458,10 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     function postToFacebook(message) {
-        const accessToken = 'EAAFsNm6cRrsBO0Fa2dWAOVLZA5Op6TRNsMmreZATYXyuqZAdQf6EMov5BmKFzmvHwBs1u87TLERRxhrqjbsnjU1ZBU4J9rmuzt1AYXKiSTPiPBh354fkHfhKrGvDUg7W5q58UIEwSZCenFtYgiorkDBjE2YfmYSVWXo1f9ldQMWwdDWUhS1QW0wZBpUjg9qUQpKkqajPqH7hVH0tjJYVoTSZCSY'; // Your Page Access Token
+        const accessToken = 'EAAHaVHQgb0cBO1ZBIs1ccyJvZBk3qCibhjK6KF4dyCEqi0dx2hWZCUGdf1HRWp2MUM3kEUntqf4WFhDpm5DIZBrYVssZCw8BcvQ03xGnll3tKYcHvBTT2lCtmeR0G0uP0Hwhk5vVSOW39vra2wXpGGCfKyZCAD0uzbPCC9dgHZA6EnQ1HiSjpPzawZC29agY0iIvVWDeFv80ZAjv97MWyBg4sVqoWzAZDZD'; // Your Page Access Token
         const page_id = '334940566378514'; 
         // const app_id = '400456016479931'; // Your Facebook App ID
-
+ 
         fetch(`https://graph.facebook.com/v20.0/${page_id}/feed`, {
             method: 'POST',
             headers: {
